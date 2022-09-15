ba8ac75e5009e5aab63bc6f3cb5ed3b09a497cfe
Irina <countess.all@yandex.ru>
2022-09-15 23:14:53 +0300

    вторая задача

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
     a = -9 b = -3 -> max = -3
 */
 
+/*
 Console.Write("введите первое число ");
 int a = int.Parse(Console.ReadLine());
 
@@ -18,5 +19,49 @@ else if (b > a)
     Console.WriteLine($"max = {b}, min = {a}");    
     else 
     Console.WriteLine("значения чисел равны");
+*/
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа
+          и выдаёт максимальное из этих чисел.
+    2, 3, 7 -> 7
+    44 5 78 -> 78
+    22 3 9 -> 22
+*/
+
+
+Console.Write("введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("введите третье число ");
+int c = int.Parse(Console.ReadLine());
+
+//вложенные простые условия
+if (a > b)
+    if(a > c)
+        Console.WriteLine(a);
+    else
+        Console.WriteLine(c);
+else
+    if(b > c)
+        Console.WriteLine(b);
+    else
+        Console.WriteLine(c);                
+
+// составные условия - логические операции
+if (a > b && a > c)
+        Console.WriteLine(a);
+else
+    if (b > a && b > c)
+        Console.WriteLine(b);
+else
+    Console.WriteLine(c); 
+
+// встроенная функция Max()
+Console.WriteLine(Math.Max(a, Math.Max(b,c)));
+
 
 
